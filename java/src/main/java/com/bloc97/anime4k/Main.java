a49f76189b8ea6d81a8121e4f037706d3722ac46
Christopher Schreiner <cs@elunic.com>
2019-10-16 11:14:46 +0200

    feat: revert to single image processing

--- java/src/main/java/com/bloc97/anime4k/Main.java
+++ java/src/main/java/com/bloc97/anime4k/Main.java
@@ -22,67 +22,59 @@ public class Main {
      * @param args the command line arguments
      */
     static ImageKernel kernel = new ImageKernel();
-
+    
     public static void main(String[] args) throws IOException {
-
+        
         if (args.length < 2) {
-            System.out.println("Error: Please specify input and output folder.");
+            System.out.println("Error: Please specify input and output png files.");
             return;
         }
-        String myDirectoryPath = args[0];
-        String outDirectoryPath = args[1];
-
-        File dir = new File(myDirectoryPath);
-        File[] directoryListing = dir.listFiles();
-        if (directoryListing != null) {
-            for (File child : directoryListing) {
-
-                BufferedImage img = ImageIO.read(child);
-                img = copyType(img);
-
-                float scale = 2f;
-
-                if (args.length >= 3) {
-                    scale = Float.parseFloat(args[2]);
-                }
-
-                float pushStrength = scale / 6f;
-                float pushGradStrength = scale / 2f;
-
-                if (args.length >= 4) {
-                    pushGradStrength = Float.parseFloat(args[3]);
-                }
-                if (args.length >= 5) {
-                    pushStrength = Float.parseFloat(args[4]);
-                }
-
-                img = scale(img, (int) (img.getWidth() * scale), (int) (img.getHeight() * scale));
-
-                kernel.setPushStrength(pushStrength);
-                kernel.setPushGradStrength(pushGradStrength);
-                kernel.setBufferedImage(img);
-                kernel.process();
-                kernel.updateBufferedImage();
-
-                ImageIO.write(img, "png", new File(outDirectoryPath + '/' + child.getName()));
-
-            }
+        
+        String inputFile = args[0];
+        String outputFile = args[1];
+        
+        BufferedImage img = ImageIO.read(new File(inputFile));
+        img = copyType(img);
+        
+        float scale = 2f;
+        
+        if (args.length >= 3) {
+            scale = Float.parseFloat(args[2]);
         }
-
+        
+        float pushStrength = scale / 6f;
+        float pushGradStrength = scale / 2f;
+        
+        if (args.length >= 4) {
+            pushGradStrength = Float.parseFloat(args[3]);
+        }
+        if (args.length >= 5) {
+            pushStrength = Float.parseFloat(args[4]);
+        }
+                
+        img = scale(img, (int)(img.getWidth() * scale), (int)(img.getHeight() * scale));
+        
+        kernel.setPushStrength(pushStrength);
+        kernel.setPushGradStrength(pushGradStrength);
+        kernel.setBufferedImage(img);
+        kernel.process();
+        kernel.updateBufferedImage();
+        
+        ImageIO.write(img, "png", new File(outputFile));
+        
+        
     }
-
     static BufferedImage copyType(BufferedImage bi) {
         BufferedImage newImage = new BufferedImage(bi.getWidth(), bi.getHeight(), BufferedImage.TYPE_INT_ARGB);
         newImage.getGraphics().drawImage(bi, 0, 0, null);
         return newImage;
     }
-
     static BufferedImage scale(BufferedImage bi, int width, int height) {
         BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2 = (Graphics2D) newImage.getGraphics();
+        Graphics2D g2 = (Graphics2D)newImage.getGraphics();
         g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
         g2.drawImage(bi, 0, 0, width, height, null);
         return newImage;
     }
-
+    
 }
